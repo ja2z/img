5fc1ffd45c83d444f8fcc1f48bd060ab70b722a4
ja2z <93806968+ja2z@users.noreply.github.com>
2021-12-08 12:58:46 -0800

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import { useMemo } from "react";
+import { client, useConfig, useElementData } from "@sigmacomputing/plugin";
+
+client.config.configureEditorPanel([
+  { name: "source", type: "element" },
+  { name: "dimension", type: "column", source: "source", allowMultiple: false },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  const config = useConfig();
+  const sigmaData = useElementData(config.source);
+  const img = useMemo(() => {
+    const dimensions = config.dimension;
+    if (sigmaData?.[dimensions]) {
+      return "https://dummyimage.com/100x50&text=" + sigmaData[dimensions][0];
+    }
+  }, [config, sigmaData]);
+
+  return <img src={img} />;
 }
 
 export default App;
